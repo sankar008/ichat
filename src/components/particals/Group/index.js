cc7954e962197c2ffa6e59688f75f7a649c3668b
Saikat Das <anjan.lutu@gmail.com>
2023-07-10 00:21:57 +0530

    Update index.js

--- src/components/particals/Group/index.js
+++ src/components/particals/Group/index.js
@@ -67,7 +67,7 @@ const Index = () => {
         <Box sx={style}>
             <ThemeProvider theme={darkTheme}>
                 <Box sx={{ px: 4, py: 1, borderBottom: '1px solid #f3f3f3' }}>
-                    <Button sx={{ position: 'absolute', right: '0', top: '5px' }} onClick={handleModClose}><IoMdClose size="20" /> &nbsp; Close</Button>
+                    <Button sx={{ position: 'absolute', right: '2em', top: '5px' }} onClick={handleModClose}><IoMdClose size="20" /> &nbsp; Close</Button>
                     <Typography id="spring-modal-title" className='' variant="h6" component="h2">
                         Create Your Album
                     </Typography>
